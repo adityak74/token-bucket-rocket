5f7a765ce51150671e1cd3506a3d334db07189c7
Aditya Karnam <akarnam37@gmail.com>
2022-07-16 01:56:55 -0500

    fix: syntax fixes, app works now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-#[macro_use] extern crate rocket;
+#[macro_use] 
+extern crate rocket;
+extern crate redis;
+
+use redis::Commands;
+use dotenv::dotenv;
+use std::env;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    dotenv().ok();
+    let redis_host = env::var("REDIS_HOST")
+        .expect("REDIS_HOST must be set");
+    let redis_conn_url = format!("redis://{}", redis_host);
+    let client = redis::Client::open(redis_conn_url);
+    let mut con = client
+        .expect("Invalid connection URL")
+        .get_connection()
+        .expect("failed to connect to Redis");
+    // throw away the result, just make sure it does not fail
+    let _ : () = con.set("my_key", 42).expect("cannot set key");
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    let value: i32 = con.get("my_key").expect("cannot get key");
+    // print!("value : {value}");
+    return value.to_string();
 }
 
 #[launch]
