603bbb900287eb38dee5937d9555aa7c64dd7718
Aditya Karnam <akarnam37@gmail.com>
2022-07-17 19:27:34 -0500

    chore: use redis client module

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 #[macro_use] 
 extern crate rocket;
 extern crate redis;
+use redis::Connection;
 
 use redis::Commands;
-use dotenv::dotenv;
-use std::env;
+use util::RedisClientInterface;
+
+mod util;
+use crate::util::RedisClient;
 
 #[get("/")]
 fn index() -> String {
-    dotenv().ok();
-    let redis_host = env::var("REDIS_HOST")
-        .expect("REDIS_HOST must be set");
-    let redis_conn_url = format!("redis://{}", redis_host);
-    let client = redis::Client::open(redis_conn_url);
-    let mut con = client
-        .expect("Invalid connection URL")
-        .get_connection()
-        .expect("failed to connect to Redis");
     // throw away the result, just make sure it does not fail
-    let _ : () = con.set("my_key", 42).expect("cannot set key");
+    let redis: RedisClientInterface = RedisClient::init();
+    let mut redis_connection: Connection = redis.get_connection();
+    let _ : () = redis_connection.set("my_key", 42).expect("cannot set key");
     // read back the key and return it.  Because the return value
     // from the function is a result for integer this will automatically
     // convert into one.
-    let value: i32 = con.get("my_key").expect("cannot get key");
+    let value: i32 = redis_connection.get("my_key").expect("cannot get key");
     // print!("value : {value}");
     return value.to_string();
 }
